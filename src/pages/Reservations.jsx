5204b530b003832b61b7952e9b0455b49bdb7fa0
Victor Leyva <vic.leyva5@gmail.com>
2026-05-11 18:18:35 -0600

    Reservations: hide out-of-stock flavors from dropdown (v1.2.6)

--- src/pages/Reservations.jsx
+++ src/pages/Reservations.jsx
@@ -470,14 +470,13 @@ export default function Reservations() {
                   className="w-full px-3 py-2 border border-gray-300 dark:border-gray-600 rounded-lg bg-white dark:bg-gray-700 text-gray-900 dark:text-white focus:outline-none focus:ring-2 focus:ring-blue-500 disabled:opacity-50"
                 >
                   <option value="">Seleccionar sabor</option>
-                  {getModelFlavors().map(flavor => {
-                    const available = getAvailableStock(flavor.id)
-                    return (
-                      <option key={flavor.id} value={flavor.id} disabled={available <= 0}>
-                        {flavor.name} - Disponible: {available}
+                  {getModelFlavors()
+                    .filter(flavor => getAvailableStock(flavor.id) > 0)
+                    .map(flavor => (
+                      <option key={flavor.id} value={flavor.id}>
+                        {flavor.name} - Disponible: {getAvailableStock(flavor.id)}
                       </option>
-                    )
-                  })}
+                    ))}
                 </select>
               </div>
 
